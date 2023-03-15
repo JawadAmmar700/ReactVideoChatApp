01c661fe05e93e0d04786c3efe001e174a245f39
JawadAmmar700 <JawadAmmarCode700@gmail.com>
2023-03-15 14:22:31 +0200

    test

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,22 +1,25 @@
 export const peerConfiguration = {
-  iceServers: [
-    {
-      urls: "stun:relay.metered.ca:80",
-    },
-    {
-      urls: "turn:relay.metered.ca:80",
-      username: process.env.REACT_APP_TURN_USERNAME,
-      credential: process.env.REACT_APP_TURN_PASSWORD,
-    },
-    {
-      urls: "turn:relay.metered.ca:443",
-      username: process.env.REACT_APP_TURN_USERNAME,
-      credential: process.env.REACT_APP_TURN_PASSWORD,
-    },
-    {
-      urls: "turn:relay.metered.ca:443?transport=tcp",
-      username: process.env.REACT_APP_TURN_USERNAME,
-      credential: process.env.REACT_APP_TURN_PASSWORD,
-    },
-  ],
+  secure: true,
+  config: {
+    iceServers: [
+      {
+        urls: "stun:relay.metered.ca:80",
+      },
+      {
+        urls: "turn:relay.metered.ca:80",
+        username: process.env.REACT_APP_TURN_USERNAME,
+        credential: process.env.REACT_APP_TURN_PASSWORD,
+      },
+      {
+        urls: "turn:relay.metered.ca:443",
+        username: process.env.REACT_APP_TURN_USERNAME,
+        credential: process.env.REACT_APP_TURN_PASSWORD,
+      },
+      {
+        urls: "turn:relay.metered.ca:443?transport=tcp",
+        username: process.env.REACT_APP_TURN_USERNAME,
+        credential: process.env.REACT_APP_TURN_PASSWORD,
+      },
+    ],
+  },
 };
